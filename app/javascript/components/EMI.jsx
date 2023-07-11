d7ccfcbff9db7d71fab34f31dbcf2f61370218cc
kirti tandulkar <kirti.tandulkar@gmail.com>
2023-07-11 16:27:45 +0530

    done

--- app/javascript/components/EMI.jsx
+++ app/javascript/components/EMI.jsx
@@ -1,78 +1,147 @@
+import { ArcElement, Chart as ChartJS, Legend, Tooltip } from "chart.js";
 import React, { useState } from "react";
+import { Pie } from "react-chartjs-2";
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const EMI = () => {
-  const [loanAmount, setLoanAmount] = useState("");
-  const [loanYears, setLoanYears] = useState("");
-  const [roi, setRoi] = useState("");
-  const [emi, setEmi] = useState("");
-  const [totalInterest, setTotalInterest] = useState("");
-  const [totalPayment, setTotalPayment] = useState("");
+  const [loanAmount, setLoanAmount] = useState(0);
+  const [numberOfYears, setNumberOfYears] = useState(0);
+  const [rateOfInterest, setRateOfInterest] = useState(0);
+  const [interest, setInterest] = useState(0);
+  const [principal, setPrincipal] = useState(0);
+  const [monthlyEmi, setMonthlyEmi] = useState(0); // New state variable
 
-  const calculateEMI = () => {
-    const principal = parseFloat(loanAmount);
-    const interestRate = parseFloat(roi) / 100 / 12;
-    const loanTerm = parseFloat(loanYears) * 12;
+  const calculateEmi = () => {
+    const principalAmount = parseInt(loanAmount);
+    const rateOfInterestPerMonth = parseFloat(rateOfInterest) / 100 / 12;
+    const loanDuration = parseInt(numberOfYears) * 12;
 
-    if (principal && interestRate && loanTerm) {
-      const x = Math.pow(1 + interestRate, loanTerm);
-      const monthlyEMI = (principal * interestRate * x) / (x - 1);
-      setEmi(monthlyEMI.toFixed(2));
+    const emi =
+      (principalAmount *
+        rateOfInterestPerMonth *
+        Math.pow(1 + rateOfInterestPerMonth, loanDuration)) /
+      (Math.pow(1 + rateOfInterestPerMonth, loanDuration) - 1);
 
-      const totalPaymentValue = monthlyEMI * loanTerm;
-      const totalInterestPayable = totalPaymentValue - principal;
-      setTotalInterest(totalInterestPayable.toFixed(2));
+    const totalInterest = emi * loanDuration - principalAmount;
+    setInterest(totalInterest.toFixed(2));
+    setPrincipal(principalAmount.toFixed(2));
+    setMonthlyEmi(emi.toFixed(2)); // Update monthly EMI
+  };
 
-      setTotalPayment(totalPaymentValue.toFixed(2));
-
-      const labels = [];
-      const data = [];
-
-      for (let i = 1; i <= loanTerm; i++) {
-        labels.push(`Month ${i}`);
-        data.push(monthlyEMI);
-      }
-    }
+  const data = {
+    labels: ["Principal Amount", "Total Interest"],
+    datasets: [
+      {
+        label: "Ratio of Principle and Interest",
+        data: [principal, interest],
+        backgroundColor: ["rgba(0, 123, 255, 0.2)", "rgba(0, 255, 255, 0.2)"],
+        borderColor: ["rgba(0, 123, 255, 1)", "rgba(0, 255, 255, 1)"],
+        hoverBackgroundColor: ["#007BFF", "#00FFFF"],
+        borderWidth: 1,
+      },
+    ],
   };
 
   return (
     <>
-      <h2>Loan Calculator</h2>
-      <div className="card" id="Card">
-        <div className="card-body">
-          <label>Loan Amount:</label>
-          <input
-            type="number"
-            value={loanAmount}
-            onChange={(e) => setLoanAmount(e.target.value)}
-            className="form-control"
-          />
+      <div className="container">
+        <h1 className="mt-4 title border-left border-3">EMI Calculator</h1>
+        <div className="row mt-4">
+          <div className="col-md-4">
+            <label className="form-label">Loan Amount:</label>
+            <input
+              type="number"
+              className="form-control rounded-0"
+              onChange={(e) => setLoanAmount(e.target.value)}
+            />
+          </div>
+
+          <div className="col-md-4">
+            <label className="form-label">Number of Years:</label>
+            <input
+              type="number"
+              className="form-control rounded-0"
+              onChange={(e) => setNumberOfYears(e.target.value)}
+            />
+          </div>
+
+          <div className="col-md-4">
+            <label className="form-label">
+              Rate of Interest (% per annum):
+            </label>
+            <input
+              type="number"
+              className="form-control rounded-0"
+              onChange={(e) => setRateOfInterest(e.target.value)}
+            />
+          </div>
+
+          <div className="mt-4">
+            <button
+              className="btn btn-dark rounded-pill"
+              onClick={calculateEmi}
+            >
+              Calculate
+            </button>
+          </div>
         </div>
-        <div className="card-body">
-          <label>No of Years:</label>
-          <input
-            type="number"
-            value={loanYears}
-            onChange={(e) => setLoanYears(e.target.value)}
-            className="form-control"
-          />
+
+        <div className="row mt-4" id="Row2">
+          <div className="col-md-4">
+            {/* <h3>EMI Calculation:</h3> */}
+            <div
+              className=" border-1 border-dark border-opacity-25"
+              id="Border"
+            >
+              <h3>Monthly EMI</h3>
+              <p>{monthlyEmi}</p>
+            </div>
+            <div
+              className=" border-1 border-dark border-opacity-25"
+              id="Border"
+            >
+              <h3>Loan Amount</h3>
+              <p> {loanAmount}</p>
+            </div>
+            <div
+              className=" border-1 border-dark border-opacity-25"
+              id="Border"
+            >
+              <h3>Number of Years</h3>
+              <p> {numberOfYears}</p>
+            </div>
+            <div
+              className=" border-1 border-dark border-opacity-25"
+              id="Border"
+            >
+              <h3>Rate of Interest</h3>
+              <p>{rateOfInterest}%</p>
+            </div>
+
+            <div
+              className=" border-1 border-dark border-opacity-25"
+              id="Border"
+            >
+              <h3>Principal Amount</h3>
+              <p>{principal}</p>
+            </div>
+
+            <div
+              className=" border-1 border-dark border-opacity-25"
+              id="Border"
+            >
+              <h3>Total Interest</h3>
+              <p>{interest}</p>
+            </div>
+          </div>
+
+          <div className="col-md-6">
+            <h3>Pie Chart:</h3>
+            <div style={{ height: "300px", width: "300px" }}>
+              <Pie data={data} />
+            </div>
+          </div>
         </div>
-        <div className="card-body">
-          <label>ROI:</label>
-          <input
-            type="number"
-            value={roi}
-            onChange={(e) => setRoi(e.target.value)}
-            className="form-control"
-          />
-        </div>
-        <div className="card-body">
-          <button onClick={calculateEMI} className="btn btn-dark">
-            Calculate
-          </button>
-        </div>
-        {emi && <p>EMI: {emi} per month</p>}
-        {totalInterest && <p>Total Interest Payable: {totalInterest}</p>}
-        {totalPayment && <p>Total Payment: {totalPayment}</p>}
       </div>
     </>
   );
